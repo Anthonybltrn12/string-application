c4a4fce7669f0043b11b5563a3dac202a7bb272c
antbeltran <anthonybltrn@gmail.com>
2026-04-08 15:19:41 -0500

    finished step 1 and 2 of excercise 2

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,5 +2,39 @@ package com.pluralsight;
 
 public class VariableApp {
 
+    public static void main(String[] args) {
+
+//question 1: step 1: declaring variables
+        String color = "blue";
+        System.out.println("My favorite color is " + color);
+
+        int yearStarted = 2026;
+        System.out.println("I started my Java course in the year " + yearStarted);
+
+        String middleInitial = "D";
+        System.out.println("The initial of my middle name is " + middleInitial);
+
+        boolean hasPets = true;
+        System.out.println("It is " + hasPets + " that i have pets");
+
+        String greeting = "Hello! How are you today?";
+        System.out.println(greeting);
+
+        //step 2; declare and initialize variables
+        int numOfWeekdays = 7;
+        System.out.println("There are " + numOfWeekdays + " days in a week");
+
+        double coffeePrice = 4.99;
+        System.out.println("The price of the coffee is " + coffeePrice);
+
+        char favoriteLetter = 'B';
+        System.out.println("My favorite letter of the alphabet is " + favoriteLetter);
+
+        boolean isRaining = true;
+        System.out.println("It is " + isRaining + " that it is raining");
+
+
+    }
+
 
 }
