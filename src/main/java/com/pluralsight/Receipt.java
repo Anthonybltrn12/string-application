6728f377c857539099e7ab3e676ffa74a4bee55d
antbeltran <anthonybltrn@gmail.com>
2026-04-08 15:30:36 -0500

    initial receipt commit

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -2,4 +2,13 @@ package com.pluralsight;
 
 public class Receipt {
 
+    public static void main(String[] args) {
+
+        String itemName = "Fairlife shake";
+        double itemPrice = 3.99;
+        int numOfItems = 2;
+
+        System.out.println("You bought " + numOfItems + " of " + itemName + " for " + "$" + (itemPrice * numOfItems));
+
+    }
 }
